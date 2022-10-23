c6f9928f30ce8ca9ee2385b0377155f0e41b11f8
Arjun Reddy <arjun.reddy@ad.starmarkit.com>
2022-10-23 10:07:07 +0530

    DB Context setup to use Entity Framework

---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI_2._2.Models;
+
+namespace WebAPI_2._2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CalenderEntry> CalenderEntries { get; set; }
+    }
+}
